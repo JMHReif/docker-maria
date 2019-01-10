39dd6b05d041504a1a0cd1dcdd919f63bd17f650
Jennifer Reif <jennifer.reif@neo4j.com>
2019-01-10 14:47:54 -0600

    Initial commit

---
+++ runMariaDB.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker run --name mymaria -p3306:3306 -d -v $HOME/Projects/docker/data/maria:/var/lib/mysql -v $HOME/Projects/docker/logs/maria:/var/log/mysql -e MYSQL_ROOT_PASSWORD=Testing123 jmreif/mariadb
+docker ps
