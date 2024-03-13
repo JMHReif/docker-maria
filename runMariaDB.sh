778623d54fac4819141750c90255dc70b972c6c8
Jennifer Reif <jennifer.reif@neo4j.com>
2024-03-13 15:38:48 -0500

    Update mariadb container and cmd

--- runMariaDB.sh
+++ runMariaDB.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker run --name mymaria -p3306:3306 -d -v $HOME/Projects/docker/data/maria:/var/lib/mysql -v $HOME/Projects/docker/logs/maria:/var/log/mysql -e MYSQL_ROOT_PASSWORD=Testing123 jmreif/mariadb
+docker run --name mymaria -p3306:3306 -d \
+    -v $HOME/Projects/docker/data/maria:/var/lib/mysql \
+    -v $HOME/Projects/docker/logs/maria:/var/log/mysql \
+    -e MYSQL_ROOT_PASSWORD=Testing123 jmreif/mariadb
 docker ps
